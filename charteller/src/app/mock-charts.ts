2b5bc6121c0f01e82f14e3b570bd4607f5fc5109
javelinsman <javelinsman@snu.ac.kr>
2018-12-19 02:11:15 +0900

    update mock charts

--- charteller/src/app/mock-charts.ts
+++ charteller/src/app/mock-charts.ts
@@ -3,9 +3,23 @@ import { Chart } from './chart';
 export const CHARTS: Chart[] = [
     {
         id: 1,
-        title: '식물호르몬 투여 실험',
-        src_png: '/assets/annotated_1.png',
-        src_json: '/assets/annotated_1.json',
-        src_svg: '/assets/annotated_1.svg'
+        title: '연도별 기업 순이익',
+        src_png: '/assets/chart_1.png',
+        src_json: '/assets/chart_1.json',
+        src_svg: '/assets/chart_1.svg'
+    },
+    {
+        id: 2,
+        title: 'OECD 국가별 자살률 순위',
+        src_png: '/assets/chart_2.png',
+        src_json: '/assets/chart_2.json',
+        src_svg: '/assets/chart_2.svg'
+    },
+    {
+        id: 3,
+        title: '월별 강수량',
+        src_png: '/assets/chart_3.png',
+        src_json: '/assets/chart_3.json',
+        src_svg: '/assets/chart_3.svg'
     }
 ];
