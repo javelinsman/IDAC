b22cebab1e13ec867f5c0257e15b414bb1ed9274
javelinsman <javelinsman@snu.ac.kr>
2019-01-04 03:05:08 +0900

    two-way binding and description rule view

--- charteller/src/app/app.module.ts
+++ charteller/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
@@ -20,7 +21,8 @@ import { KeyboardInputComponent } from './keyboard-input/keyboard-input.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
