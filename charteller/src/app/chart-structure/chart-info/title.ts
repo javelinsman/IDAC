2884a4a5f832d459a5c82e9dc431130dca0c3158
javelinsman <javelinsman@snu.ac.kr>
2019-01-03 23:40:16 +0900

    Update title.ts

--- charteller/src/app/chart-structure/chart-info/title.ts
+++ charteller/src/app/chart-structure/chart-info/title.ts
@@ -1,6 +1,5 @@
 import { ChartAccent } from '../chart-accent/chart-accent';
 import { Tag } from './tag';
-import { ChartInfo } from './chart-info';
 
 export class Title extends Tag {
   constructor(ca: ChartAccent) {
