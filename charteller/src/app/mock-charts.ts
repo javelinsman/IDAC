7211834cccf97f929bb40f8b0cf80f63420fcfd1
javelinsman <javelinsman@snu.ac.kr>
2019-01-04 11:40:43 +0900

    black magic

--- charteller/src/app/mock-charts.ts
+++ charteller/src/app/mock-charts.ts
@@ -4,22 +4,22 @@ export const CHARTS: Chart[] = [
     {
         id: 1,
         title: 'Net Income of Companies',
-        src_png: '/assets/chart_1_eng.png',
-        src_json: '/assets/chart_1_eng.json',
-        src_svg: '/assets/chart_1_eng.svg'
+        src_png: '/Charteller/assets/chart_1_eng.png',
+        src_json: '/Charteller/assets/chart_1_eng.json',
+        src_svg: '/Charteller/assets/chart_1_eng.svg'
     },
     {
         id: 2,
         title: 'Suicide Rate among OECD Countries',
-        src_png: '/assets/chart_2_eng.png',
-        src_json: '/assets/chart_2_eng.json',
-        src_svg: '/assets/chart_2_eng.svg'
+        src_png: '/Charteller/assets/chart_2_eng.png',
+        src_json: '/Charteller/assets/chart_2_eng.json',
+        src_svg: '/Charteller/assets/chart_2_eng.svg'
     },
     {
         id: 3,
         title: 'Precipitation by Month',
-        src_png: '/assets/chart_3_eng.png',
-        src_json: '/assets/chart_3_eng.json',
-        src_svg: '/assets/chart_3_eng.svg'
+        src_png: '/Charteller/assets/chart_3_eng.png',
+        src_json: '/Charteller/assets/chart_3_eng.json',
+        src_svg: '/Charteller/assets/chart_3_eng.svg'
     }
 ];
