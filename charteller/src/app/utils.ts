b15f59bd6da2b85186c4e8470ecf48e3340bed16
javelinsman <javelinsman@snu.ac.kr>
2019-01-15 15:02:16 +0900

    Update utils.ts

--- charteller/src/app/utils.ts
+++ charteller/src/app/utils.ts
@@ -1,46 +1,50 @@
-export function eqArray(a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length != b.length) return false;
+export function eqArray(a: any[], b: any[]) {
+    if (a === b) {
+      return true;
+    } else if (a === null || b === null) {
+      return false;
+    } else if (a.length !== b.length) {
+      return false;
+    }
 
-    // If you don't care about the order of the elements inside
-    // the array, you should sort both arrays here.
-    // Please note that calling sort on an array will modify that array.
-    // you might want to clone your array first.
-
-    for (var i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
+    for (let i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
     }
     return true;
 }
 
-export function eqSet(as, bs) {
-    if (as.size !== bs.size) return false;
-    let eq = [];
+export function eqSet(as: Set<any>, bs: Set<any>) {
+    if (as.size !== bs.size) {
+      return false;
+    }
+    const eq = [];
     as.forEach(a => eq.push(bs.has(a)));
-    return eq.reduce((a,b) => a&&b);
+    return eq.reduce((a, b) => a && b);
 }
 
-let a=new AudioContext() // browsers limit the number of concurrent audio contexts, so you better re-use'em
+const audioContext = new AudioContext();
+// browsers limit the number of concurrent audio contexts, so you better re-use'em
 
-export function beep(vol, freq, duration){
-  let v=a.createOscillator()
-  let u=a.createGain()
-  v.connect(u)
-  v.frequency.value=freq
-  v.type="square"
-  u.connect(a.destination)
-  u.gain.value=vol*0.01
-  v.start(a.currentTime)
-  v.stop(a.currentTime+duration*0.001)
+export function beep(vol, freq, duration) {
+  const v = audioContext.createOscillator();
+  const u = audioContext.createGain();
+  v.connect(u);
+  v.frequency.value = freq;
+  v.type = 'square';
+  u.connect(audioContext.destination);
+  u.gain.value = vol * 0.01;
+  v.start(audioContext.currentTime);
+  v.stop(audioContext.currentTime + duration * 0.001);
 }
 
-export function beep_error(){
-  beep(5, 700, 150)
+export function beep_error() {
+  beep(5, 700, 150);
 }
 
-export function beep_detect(){
-  beep(5, 350, 150)
+export function beep_detect() {
+  beep(5, 350, 150);
 }
 
 let sayTimeout = null;
@@ -66,16 +70,20 @@ export function speak(message, korean = false) {
   }
 }
 
-export function isAscendingArray(arr){
-  for(let i=1;i<arr.length;i++){
-    if(arr[i-1] > arr[i]) return false;
+export function isAscendingArray(arr: any[]) {
+  for (let i = 1; i < arr.length ; i++) {
+    if (arr[i - 1] > arr[i]) {
+      return false;
+    }
   }
-  return true
+  return true;
 }
 
-export function isDescendingArray(arr){
-  for(let i=1;i<arr.length;i++){
-    if(arr[i-1] < arr[i]) return false;
+export function isDescendingArray(arr: any[]) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] < arr[i]) {
+      return false;
+    }
   }
-  return true
+  return true;
 }
