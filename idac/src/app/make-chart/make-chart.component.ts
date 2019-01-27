97da56eef280511b3e9fe5c277124f6788de210c
javelinsman <javelinsman@snu.ac.kr>
2019-01-27 18:37:13 +0900

    fetch ChartAccent spec json

--- idac/src/app/make-chart/make-chart.component.ts
+++ idac/src/app/make-chart/make-chart.component.ts
@@ -6,6 +6,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ChartExampleService } from '../chart-example.service';
 import * as d3 from 'd3';
 import { Chart } from '../chart';
+import { ChartAccent } from '../chart-structure/chart-accent/chart-accent';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-make-chart',
@@ -14,21 +16,34 @@ import { Chart } from '../chart';
 })
 export class MakeChartComponent implements OnInit {
   chart: Chart;
+  chartAccent: ChartAccent;
 
   constructor(
       private chartExampleService: ChartExampleService,
-      private route: ActivatedRoute
+      private route: ActivatedRoute,
+      private http: HttpClient
     ) { }
 
   ngOnInit() {
     const exampleId = +this.route.snapshot.paramMap.get('exampleId');
     if (exampleId) {
-      this.fetchExampleData(exampleId);
+      this.chart = this.fetchExampleChart(exampleId);
+    } else {
+      this.chart = this.fetchChart();
     }
+
+    this.http.get<ChartAccent>(this.chart.src_json).subscribe(data => {
+      this.chartAccent = data;
+    });
+
   }
 
-  fetchExampleData(id: number) {
-    this.chart = this.chartExampleService.getCharts()[id];
+  fetchExampleChart(id: number) {
+    return this.chartExampleService.getCharts()[id];
+  }
+
+  fetchChart() {
+    return this.chartExampleService.getCharts()[0];
   }
 
 }
