0e36ab8a6658d88580d59590507160dbdc5a190d
javelinsman <javelinsman@snu.ac.kr>
2019-01-30 19:23:24 +0900

    fuzzy search

--- idac/src/app/panels/description-panel/description-panel.component.ts
+++ idac/src/app/panels/description-panel/description-panel.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { SpecTag } from 'src/app/chart-structure/chart-spec/spec-tag';
+import * as FuzzySearch from 'fuzzy-search';
+
 
 @Component({
   selector: 'app-description-panel',
@@ -10,9 +12,33 @@ export class DescriptionPanelComponent implements OnInit {
 
   @Input() tag: SpecTag;
 
+  @ViewChild('input') textarea: ElementRef<HTMLTextAreaElement>;
+
+  fuzzySearcher: FuzzySearch;
+
   constructor() { }
 
   ngOnInit() {
+    const keys = Object.keys(this.tag.properties);
+    this.fuzzySearcher = new FuzzySearch(keys.map(key => ({ key })), ['key']);
+  }
+
+  searchKeyword(keyword: string): string[] {
+    return this.fuzzySearcher.search(keyword).map(({key}) => key);
+  }
+
+
+  onInput() {
+    const content = this.textarea.nativeElement.value;
+    const pos = this.textarea.nativeElement.selectionStart;
+    const leftContent = content.slice(0, pos),
+          rightContent = content.slice(pos);
+    const regexIsOpen = /^([^\$\(\)]*\$\([^\$\(\)]*\)[^\$\(\)]*)*[^\$\(\)]*\$\((?<cand>[^\$\(\)]*)$/;
+    const match = leftContent.match(regexIsOpen);
+    if (match) {
+      const keyword = match.groups.cand + rightContent.split(' ')[0].split(')')[0];
+      console.log(this.searchKeyword(keyword));
+    }
   }
 
 }
