a97d5fb97a54359e317aa4e11cf746b457405a69
javelinsman <javelinsman@snu.ac.kr>
2019-02-07 13:52:40 +0900

    add edit button in property panel

--- idac/src/app/panels/properties-panel/properties-panel.component.ts
+++ idac/src/app/panels/properties-panel/properties-panel.component.ts
@@ -8,6 +8,7 @@ import { SpecTag } from 'src/app/chart-structure/chart-spec/spec-tag';
 })
 export class PropertiesPanelComponent implements OnInit {
   @Input() tag: SpecTag;
+  edit = {};
 
   constructor() { }
 
@@ -16,6 +17,9 @@ export class PropertiesPanelComponent implements OnInit {
   }
 
   ngOnInit() {
+    Object.keys(this.tag.attributes).forEach(key => {
+      this.edit[key] = false;
+    });
   }
 
   onDragStart(event: DragEvent) {
