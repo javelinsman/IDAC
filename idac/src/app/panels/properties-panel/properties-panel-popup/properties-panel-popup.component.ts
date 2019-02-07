422954f9a3bbaa8c21a6ee94387302c22d62ccd4
javelinsman <javelinsman@snu.ac.kr>
2019-02-07 16:21:55 +0900

    make all properties panel draggable

--- idac/src/app/panels/properties-panel/properties-panel-popup/properties-panel-popup.component.ts
+++ idac/src/app/panels/properties-panel/properties-panel-popup/properties-panel-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { SpecTag } from 'src/app/chart-structure/chart-spec/spec-tag';
 
 @Component({
@@ -9,7 +9,13 @@ import { SpecTag } from 'src/app/chart-structure/chart-spec/spec-tag';
 export class PropertiesPanelPopupComponent implements OnInit {
 
   @Input() tag: SpecTag;
+  @ViewChild('cardSection') cardSection: ElementRef;
   edit = {};
+  dragging = false;
+  offset = {
+    x: 0,
+    y: 0
+  };
 
   constructor() { }
 
@@ -17,11 +23,33 @@ export class PropertiesPanelPopupComponent implements OnInit {
     Object.keys(this.tag.attributes).forEach(key => {
       this.edit[key] = false;
     });
+    this.cardSection.nativeElement.style.left = '300px';
+    this.cardSection.nativeElement.style.top = '300px';
   }
 
   isAttribute(key: string) {
     return Object.keys(this.tag.attributes).includes(key);
   }
 
+  onPanelMousedown(event: MouseEvent) {
+    event.preventDefault();
+    this.dragging = true;
+    this.offset.x = event.clientX - parseInt(this.cardSection.nativeElement.style.left, 10);
+    this.offset.y = event.clientY - parseInt(this.cardSection.nativeElement.style.top, 10);
+    document.onmousemove = this.onPanelMousemove.bind(this);
+  }
 
+  onPanelMouseup(event: MouseEvent) {
+    event.preventDefault();
+    this.dragging = false;
+    document.onmousemove = null;
+  }
+
+  onPanelMousemove(event: MouseEvent) {
+    event.preventDefault();
+    if (this.dragging) {
+      this.cardSection.nativeElement.style.left = `${event.clientX - this.offset.x}px`;
+      this.cardSection.nativeElement.style.top = `${event.clientY - this.offset.y}px`;
+    }
+  }
 }
