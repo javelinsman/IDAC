481a2e25f77b9025d632fb6dbe0a79fc55445117
javelinsman <javelinsman@snu.ac.kr>
2019-02-14 03:02:14 +0900

    highlight undefined in description

--- idac/src/app/chart-spec/chart-spec-tree-view/description/chart-spec-tree-view-description.component.ts
+++ idac/src/app/chart-spec/chart-spec-tree-view/description/chart-spec-tree-view-description.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterContentChecked, ViewChild, ElementRef } from '@angular/core';
 import { SpecTag } from 'src/app/chart-structure/chart-spec/spec-tag';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-chart-spec-tree-view-description',
   templateUrl: './chart-spec-tree-view-description.component.html',
   styleUrls: ['./chart-spec-tree-view-description.component.scss']
 })
-export class ChartSpecTreeViewDescriptionComponent implements OnInit {
+export class ChartSpecTreeViewDescriptionComponent implements OnInit, AfterContentChecked {
 
   @Input() viewOnly: boolean;
   @Input() tag: SpecTag;
@@ -14,11 +15,23 @@ export class ChartSpecTreeViewDescriptionComponent implements OnInit {
   @Input() edit: boolean;
   @Input() _editChange: any;
 
+  @ViewChild('description') descriptionP: ElementRef<HTMLParagraphElement>;
+
   hover: boolean;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentChecked() {
+    const description =
+      (this.tag.describe().length
+        ? this.tag.describe()
+        : '(No description)'
+      ).replace(/undefined/g, '<span class="undefined-variable">undefined</span>')
+    d3.select(this.descriptionP.nativeElement)
+      .html(description)
   }
 
 }
