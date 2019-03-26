7522d7c5478e9bbbb59120274bc9a7a1e7bb4a68
javelinsman <javelinsman@snu.ac.kr>
2019-03-26 14:23:03 +0900

    모달을 제 위치에 놓기 위한 노력

--- idac/src/app/chart-spec/chart-spec-tree-view/add-annotation/chart-spec-tree-view-add-annotation.component.ts
+++ idac/src/app/chart-spec/chart-spec-tree-view/add-annotation/chart-spec-tree-view-add-annotation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-chart-spec-tree-view-add-annotation',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartSpecTreeViewAddAnnotationComponent implements OnInit {
 
-  constructor() { }
+  modalCloseResult: string = '';
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  open(content) {
+    this.modalService.open(content, {
+        ariaLabelledBy: 'modal-basic-title',
+        backdrop: false, // TODO: enable it and resolve CSS problem
+        backdropClass: 'add-annotation',
+        windowClass: 'add-annotation',
+        container: '#add-annotation-modal-container'
+      })
+      .result.then((result) => {
+      this.modalCloseResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.modalCloseResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
